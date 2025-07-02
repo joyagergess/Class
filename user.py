dbe77bbe716cf71cd71e24078144d8ec6cc75feb
joyagergess <gergessjoya@gmail.com>
2025-07-02 17:30:48 +0300

    Added class Student

--- user.py
+++ user.py
@@ -9,3 +9,15 @@ class User:
     def set_email(self, new_email):
         self.__email = new_email
 
+
+class Student(User):
+    def __init__(self, name, email):
+        super().__init__(name, email)
+        self.__enrolled_courses = []
+
+    def enroll(self, course_name):
+        self.__enrolled_courses.append(course_name)
+
+    def get_enrolled_courses(self):
+        return self.__enrolled_courses
+
